ac7a0ae988ca2dc448ef93d1fdb6c3dc71bf956c
Ricky Elrod <ricky@elrod.me>
2016-09-08 14:28:11 -0400

    noexc
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- scripts/deploy-haddock-manually.sh
+++ scripts/deploy-haddock-manually.sh
@@ -3,7 +3,7 @@
 cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
 cd "$cwd/.."
 f=`mktemp -d`
-git clone "git@github.com:relrod/nmea0183.git" "$f/nmea0183.git"
+git clone "git@github.com:noexc/nmea0183.git" "$f/nmea0183.git"
 cabal haddock
 pushd "$f/nmea0183.git"
   git checkout gh-pages && git rm -rf *
@@ -17,7 +17,7 @@ popd
 rm -rf "$f"
 
 if [ $? == 0 ]; then
-  echo "*** Done: https://relrod.github.io/nmea0183/"
+  echo "*** Done: https://noexc.github.io/nmea0183/"
   exit 0
 else
   echo "*** ERROR!!! Fix the above and try again."
