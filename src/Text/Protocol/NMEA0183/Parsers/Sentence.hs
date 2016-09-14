a86f7d26ba5178c59a43518dfd4c425cadda4c75
Ricky Elrod <ricky@elrod.me>
2016-09-13 22:21:22 -0400

    Add talkerids, based on http://catb.org/gpsd/NMEA.html

--- src/Text/Protocol/NMEA0183/Parsers/Sentence.hs
+++ src/Text/Protocol/NMEA0183/Parsers/Sentence.hs
@@ -8,43 +8,72 @@ import Text.Parser.Char
 import Text.Parser.Combinators
 
 parseTalkerId :: (Monad m, CharParsing m) => m TalkerIdentifier
-parseTalkerId = do
-  x <- string "P" <|> count 2 upper
-  return $ case x of
-    "AG" -> AG
-    "AP" -> AP
-    "CD" -> CD
-    "CR" -> CR
-    "CS" -> CS
-    "CT" -> CT
-    "CV" -> CV
-    "CX" -> CX
-    "DF" -> DF
-    "EC" -> EC
-    "EP" -> EP
-    "ER" -> ER
-    "GP" -> GP
-    "HC" -> HC
-    "HE" -> HE
-    "HN" -> HN
-    "II" -> II
-    "IN" -> IN
-    "LC" -> LC
-    "P"  -> P
-    "RA" -> RA
-    "SD" -> SD
-    "SN" -> SN
-    "SS" -> SS
-    "TI" -> TI
-    "VD" -> VD
-    "DM" -> DM
-    "VW" -> VW
-    "WI" -> WI
-    "YX" -> YX
-    "ZA" -> ZA
-    "ZC" -> ZC
-    "ZQ" -> ZQ
-    "ZV" -> ZV
+parseTalkerId = parseU <|> parseNormal
+  where
+    parseU = do
+      _ <- char 'U'
+      n <- digit
+      -- This 'read' is bad (partial), but we are relying on "parsers" indeed
+      -- giving us a digit back (the parser should/will fail otherwise).
+      -- Nevertheless, we should possibly 'readMay' and fail ourselves.
+      return (U (read [n]))
+    parseNormal = do
+      x <- count 2 upper
+      return $ case x of
+        "AB" -> AB
+        "AD" -> AD
+        "AG" -> AG
+        "AP" -> AP
+        "BN" -> BN
+        "CC" -> CC
+        "CD" -> CD
+        "CM" -> CM
+        "CS" -> CS
+        "CT" -> CT
+        "CV" -> CV
+        "CX" -> CX
+        "DE" -> DE
+        "DF" -> DF
+        "DU" -> DU
+        "EC" -> EC
+        "EP" -> EP
+        "ER" -> ER
+        "GP" -> GP
+        "HC" -> HC
+        "HE" -> HE
+        "HN" -> HN
+        "II" -> II
+        "IN" -> IN
+        "LA" -> LA
+        "LC" -> LC
+        "MP" -> MP
+        "NL" -> NL
+        "OM" -> OM
+        "OS" -> OS
+        "RA" -> RA
+        "SD" -> SD
+        "SN" -> SN
+        "SS" -> SS
+        "TI" -> TI
+        "TR" -> TR
+        "UP" -> UP
+        "VD" -> VD
+        "DM" -> DM
+        "VW" -> VW
+        "WI" -> WI
+        "YC" -> YC
+        "YD" -> YD
+        "YF" -> YF
+        "YL" -> YL
+        "YP" -> YP
+        "YR" -> YR
+        "YT" -> YT
+        "YV" -> YV
+        "YX" -> YX
+        "ZA" -> ZA
+        "ZC" -> ZC
+        "ZQ" -> ZQ
+        "ZV" -> ZV
 
 parseSentenceIdentifier :: CharParsing m => m T.Text
 parseSentenceIdentifier = T.pack <$> count 3 upper
